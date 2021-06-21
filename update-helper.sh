5c6580396ce2ef8eae1d0b7a5520929706c1f235
nhatthanhxx <nhatthanhxx@gmail.com>
2021-06-21 11:29:55 +0700

    Update update-helper.sh

--- update-helper.sh
+++ update-helper.sh
@@ -4,7 +4,7 @@ dir=Mayoi-ecchi
 tempdir=Mayoi_Temp
 olddir=Mayoi_old
 
-while :; do echo "" && eccho "Run bot" && cd "$root/$dir" && node app.js && cd "$root" && wget -N https://github.com/nhatthanhxx/Mayoi-ecchi/raw/main/self-update.sh && bash "$root/self-update.sh"; sleep 5s; done
+while :; do echo "" && echo "Run bot" && cd "$root/$dir" && node app.js && cd "$root" && wget -N https://github.com/nhatthanhxx/Mayoi-ecchi/raw/main/self-update.sh && bash "$root/self-update.sh"; sleep 5s; done
 
 echo ""
 echo "If you can read this, somethings wrong and node restart."
