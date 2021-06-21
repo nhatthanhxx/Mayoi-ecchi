e0d8cb6675bf597231b5e80eb2e47251c33ef900
nhatthanhxx <nhatthanhxx@gmail.com>
2021-06-21 12:18:27 +0700

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,9 +1,10 @@
+const Discord = require("discord.js");
+
 exports.run = async (client, msg, args) => {
     if(msg.author.id == client.config.OWNERS) {
         msg.channel.send("Mayoi will be rebooted.");
         client.destroy();
-    }
-
+    };
 }
 
 exports.conf = {
