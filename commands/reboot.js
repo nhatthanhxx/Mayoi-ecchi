43d46d28d4b6e4ebdab29ed38f24733488debd3c
nhatthanhxx <nhatthanhxx@gmail.com>
2021-06-21 11:27:45 +0700

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,6 +1,9 @@
 exports.run = async (client, msg, args) => {
-    if(msg.author.id == client.config.OWNERS)
-    client.destroy();
+    if(msg.author.id == client.config.OWNERS) {
+        msg.channel.send("Mayoi will be rebooted.");
+        client.destroy();
+    }
+
 }
 
 exports.conf = {
