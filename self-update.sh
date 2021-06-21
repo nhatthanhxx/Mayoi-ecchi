b0053d0d5e5ec2629ea0bfe3b87f55f0bf94b38b
nhatthanhxx <nhatthanhxx@gmail.com>
2021-06-21 10:52:34 +0700

    fix updater

--- self-update.sh
+++ self-update.sh
@@ -6,27 +6,28 @@ olddir=Mayoi_old
 
 cd "$root"
 rm -r -f "$tempdir"
-mkdir "$tempdir"
-cd "$tempdir"
-git clone https://github.com/nhatthanhxx/Mayoi-ecchi.git
+git clone https://github.com/nhatthanhxx/Mayoi-ecchi.git "$tempdir"
 echo ""
 echo "Clone done"
 
-cd "$root"
-
 mv -f "$dir" "$olddir"
-mv "$tempdir/$dir" "$dir"
+mv -f "$tempdir" "$dir"
 echo ""
 echo "Replace done"
 
 cp -f "$root/$olddir/config.json" "$root/$dir/config.json"
-cp -f "$root/$olddir/data" "$root/$dir/data"
+cp -f -r "$root/$olddir/data" "$root/$dir/data"
 echo ""
 echo "Copy data done"
 
 rm -r -f "$tempdir"
 echo ""
 echo "Clean done"
+
+cd "$root"
+cp "$root/$dir/update-helper.sh" "$root/update-helper.sh"
+echo ""
+echo "Update local bash"
 
 cd "$root"
 rm "$root/self-update.sh"
--- update-helper.sh
+++ update-helper.sh
@@ -7,6 +7,6 @@ olddir=Mayoi_old
 while :; do cd "$root/$dir" && node app.js && cd "$root" && wget -N https://github.com/nhatthanhxx/Mayoi-ecchi/raw/main/self-update.sh && bash "$root/self-update.sh"; sleep 5s; done
 
 echo ""
-echo "If you can read this, somethings wrong."
+echo "If you can read this, somethings wrong and node restart."
 
 exit 0
